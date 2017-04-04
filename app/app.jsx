774d51fc56358d035b4d4da6ba6b24d8a7011965
Mike Murray <michaelbrazell@gmail.com>
2017-04-03 22:30:17 -0400

    Wrap rendering in global function, export as global renderGroups()

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var GroupCard = require('GroupCard');
 
-ReactDOM.render(
-  <GroupCard />,
-  document.getElementById('group_card')
-);
+function renderGroups() {
+  ReactDOM.render(
+    <GroupCard />,
+    document.getElementById('group_card')
+  );
+}
+
+module.exports = renderGroups;
+window.renderGroups = renderGroups;
