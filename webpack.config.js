09788b122d3ed3872c9e56e8ae644664eb469ccd
Mike Murray <michaelbrazell@gmail.com>
2018-01-04 13:05:59 -0500

    Update react, react dom, react router, babel-jest.  Tests passing

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2016']
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
